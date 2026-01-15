e510ce83d9e9e10dc3491dae170c74351c74b1b3
joecqupt <joe469391363@gmail.com>
2026-01-15 23:39:06 +0800

    refactor：remove unused imports and comments

--- api/src/main/java/io/github/xinfra/lab/remoting/message/MessageHandler.java
+++ api/src/main/java/io/github/xinfra/lab/remoting/message/MessageHandler.java
@@ -1,12 +1,8 @@
 package io.github.xinfra.lab.remoting.message;
 
 import io.netty.channel.ChannelHandlerContext;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public interface MessageHandler {
-
-	Logger log = LoggerFactory.getLogger(MessageHandler.class);
 
 	void registerMessageTypeHandler(MessageTypeHandler messageTypeHandler);
 
